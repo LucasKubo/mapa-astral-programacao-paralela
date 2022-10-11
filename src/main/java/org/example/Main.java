ebed99ed80e849a51b383ba145be34615c0437cb
LucasKubo <lucaskbk2@gmail.com>
2022-10-11 19:53:14 -0300

    implementada leitura paralela de arquivos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,10 +7,10 @@ import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
 
         String HOME_PATH = System.getProperty("user.dir");
         System.out.println(HOME_PATH);
@@ -19,28 +19,28 @@ public class Main {
         Path path = Paths.get(participantes);
         List<String> lines = Files.readAllLines(path);
         List<Pessoa> pessoaList = new ArrayList<>();
-        lines.forEach(line -> {
-            String[] campos = line.split(",");
-            String nome = campos[0];
-            String zona = campos[1];
-            LocalDate dataNascimento = LocalDate.parse(campos[2]);
-
-            Pessoa pessoa = new Pessoa(nome, zona, dataNascimento);
-            pessoaList.add(pessoa);
-        });
-
-
-        pessoaList.stream()
+        lines.stream()
                 .parallel()
-                .forEach((pessoa) -> {
-                    System.out.println(Thread.currentThread().getName() + System.nanoTime());
-                    String caminho = HOME_PATH.concat("/files/" + pessoa.getNome() + ".txt");
-                    try {
-                        Arquivo.criar(caminho, pessoa.gerarRelatorio());
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
+                .forEach(line -> {
+                    String[] campos = line.split(",");
+                    String nome = campos[0];
+                    String zona = campos[1];
+                    LocalDate dataNascimento = LocalDate.parse(campos[2]);
+
+                    Pessoa pessoa = new Pessoa(nome, zona, dataNascimento);
+                    pessoaList.add(pessoa);
                 });
 
+        pessoaList.stream()
+                    .parallel()
+                    .forEach((pessoa) -> {
+                        System.out.println(Thread.currentThread().getName() + System.nanoTime());
+                        String caminho = HOME_PATH.concat("/files/" + pessoa.getNome() + ".txt");
+                        try {
+                            Arquivo.criar(caminho, pessoa.gerarRelatorio());
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
     }
 }
